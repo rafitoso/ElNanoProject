2060f6c8acff6423e8b9641de28ef5271f91f1bc
Sophie Chauvier <sophiechauvier@MacBook-Air-de-Sophie.local>
2016-06-03 01:21:15 +0200

    user bundle + basic design

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,22 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        // return $this->render('default/index.html.twig', array(
+        //     'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        // ));
+        return array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..')
+            );
     }
 }
---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+namespace UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table("User")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
